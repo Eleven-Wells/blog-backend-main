37aab7d0a3f8e503e26f2d1e1afb620d76ad780f
hxscyprodz <hxscyprodz@gmail.com>
2026-04-24 10:38:25 +0200

    Add database connection config

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "./services/logger.service";
 import config from "./config/environments";
+import connectDB from "./config/db";
 
 const app = express();
 const port = 3000;
@@ -11,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const startServer = async() => {
     try{
+        await connectDB();
         app.listen(config.PORT, () => logger.info(`Server listening on port ${port}...`));
     } catch(error: any) {
         console.log(error);
